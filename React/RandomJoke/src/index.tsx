06470243dc2bcadadc26816dbce8bbca0601de53
Kangrok Kim <kimpro@uebermensch.tech>
2024-07-29 23:36:38 +0900

    Create src/index.tsx

---
+++ React/RandomJoke/src/index.tsx
@@ -0,0 +1,9 @@
+// Today's Random Joke
+// 2024.07.29
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import '/style.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
